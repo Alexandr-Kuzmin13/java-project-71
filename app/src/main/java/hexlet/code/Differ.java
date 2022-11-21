4190b7f102d9650fc9a55a85c6d23c58c3c22b20
Alexandr-Kuzmin13 <rusglagol@gmail.com>
2022-11-21 15:33:29 +0300

    number 7

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -37,7 +37,32 @@ public class Differ {
         return Formatter.formatter(result, format);
 
     }
+    public static String generate(String filepath1, String filepath2) throws IOException {
 
+        HashMap<String, Object> data1 = Parser.parsing(filepath1);
+        HashMap<String, Object> data2 = Parser.parsing(filepath2);
+        LinkedHashMap<String, Object> result = new LinkedHashMap<>();
 
+        assert data1 != null;
+        TreeSet<String> keys = new TreeSet<>(data1.keySet());
+        assert data2 != null;
+        keys.addAll(data2.keySet());
 
+        for (String key: keys) {
+
+            if (!data1.containsKey(key)) {
+                result.put("+ " + key, data2.get(key));
+            } else if (!data2.containsKey(key)) {
+                result.put("- " + key, data1.get(key));
+            } else if (Objects.equals(data1.get(key), data2.get(key))) {
+                result.put("  " + key, data1.get(key));
+            } else if (!Objects.equals(data1.get(key), data2.get(key))) {
+                result.put("- " + key, data1.get(key));
+                result.put("+ " + key, data2.get(key));
+            }
+        }
+
+        return Formatter.formatter(result, "stylish");
+
+    }
 }
